09ee9af2088e3737d6900d8e880339c02fa8f33a
7200983958 <144588848+7200983958@users.noreply.github.com>
2023-09-25 19:35:18 +0530

    Update main (8).py

--- main (8).py
+++ main (8).py
@@ -1,24 +1,22 @@
 class Student:
-  def __init__(self,name,  roll_number,cgpa): 
-     self.name=name                      self.roll_number=roll_number 
-     self.cgpa = cgpa 
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
 
 def sort_students(student_list):
-    # Sort the list of students in descending order of CGPA 
-      sorted_students=sorted(student_list,key=lambda student:student.cgpa, reverse=True)   return sorted_students 
+  sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+  return sorted_students
 
+students = [
+  Student("Priya", "A123", 7.8),
+  Student("Ramya", "A124", 8.9),
+  Student("Kamal", "A125", 9.1),
+  Student("Gokul", "A126", 9.9),  
+]
 
+sorted_students = sort_students(students)
 
-# Example usuage:
-students=[
-  Student("Hari","A 123",7.8),
-  Student("Srikanth","A 124",8.9),
-  Student("Sowmiya","A 125",9.1),
-  Student("Mohana","A 126",9.9), 
-] 
-sorted_students = sort_students(students) 
-
-# Print the sorted List of students 
 for student in sorted_students:
-  print("Name:{}, Roll Number:{}, CGPA:{}".format(student.name, student.roll_number,
-                                                  student.cgpa))
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
